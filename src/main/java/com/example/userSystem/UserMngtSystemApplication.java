ec49d58e352e392858fdcd7fb257f104d1fc1d98
socheatathai <socheatathai62@gmail.com>
2023-07-12 01:11:04 +0700

    first Commit

---
+++ src/main/java/com/example/userSystem/UserMngtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.userSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMngtSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMngtSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/userSystem/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.userSystem.service;
+
+import com.example.userSystem.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    List<User> getAllUsers();
+    User getUserById(Long id);
+
+    boolean deleteUser(Long id);
+
+    User updateUser(Long id, User user);
+}
